5f4d0698af098c429c45ae360643f355bf5e2fb3
L <=>
2017-07-13 04:06:47 -0500

    add: noise to header

--- src/FusionEKF.h
+++ src/FusionEKF.h
@@ -32,6 +32,7 @@ public:
   KalmanFilter ekf_;
 
 private:
+
   // check whether the tracking toolbox was initialized or not (first measurement)
   bool is_initialized_;
 
@@ -44,6 +45,10 @@ private:
   Eigen::MatrixXd R_radar_;
   Eigen::MatrixXd H_laser_;
   Eigen::MatrixXd Hj_;
+
+  //acceleration noise components
+	float noise_ax;
+	float noise_ay;
 };
 
 #endif /* FusionEKF_H_ */
